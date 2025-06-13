5b7a18a6fd8b23d145e998e954809c6fbf48cc35
Miguel Angel Califa Urquiza <miguel5612@users.noreply.github.com>
2025-06-13 09:34:18 -0500

    Fix checkbox event typing

---
+++ frontend/src/app/services/project.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { Project, ProjectCreate } from '../models';
+
+@Injectable({ providedIn: 'root' })
+export class ProjectService {
+  constructor(private api: ApiService) {}
+
+  getProjects(): Observable<Project[]> {
+    return this.api.getProjects();
+  }
+
+  getProject(id: number): Observable<Project> {
+    return this.api.getProject(id);
+  }
+
+  createProject(project: ProjectCreate): Observable<Project> {
+    return this.api.createProject(project);
+  }
+
+  updateProject(id: number, project: ProjectCreate): Observable<Project> {
+    return this.api.updateProject(id, project);
+  }
+
+  deleteProject(id: number): Observable<any> {
+    return this.api.deleteProject(id);
+  }
+
+  assignAnalyst(projectId: number, userId: number): Observable<Project> {
+    return this.api.assignAnalyst(projectId, userId);
+  }
+
+  unassignAnalyst(projectId: number, userId: number): Observable<Project> {
+    return this.api.unassignAnalyst(projectId, userId);
+  }
+}
